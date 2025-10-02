411e5224b1e9a4b11429c0b6c5363ea2a1e8eb23
jxie9 <jaxie963@gmail.com>
2025-10-02 02:10:09 -0500

    Bulk push magic files

---
+++ setup.sh
@@ -0,0 +1,5 @@
+export OPENLANE_ROOT=$(pwd)/caravel_user_project_analog/dependencies/openlane_src
+export PDK=sky130B
+export PDK_ROOT=$(pwd)/caravel_user_project_analog/dependencies/pdks 
+export PATH="/home/jason/.local/bin:$PATH"
+export CARAVEL_ROOT=$(pwd)/caravel_user_project_analog/caravel
